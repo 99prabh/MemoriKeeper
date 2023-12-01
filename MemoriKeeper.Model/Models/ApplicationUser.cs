bfc135004afbf7d0247cd54357d74a04929f504b
99prabh <133125732+99prabh@users.noreply.github.com>
2023-12-01 15:14:09 -0500

    Added application and directory entry domain class

---
+++ MemoriKeeper.Model/Models/ApplicationUser.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace MemoriKeeper.Model.Models
+{
+    public class ApplicationUser : IdentityUser
+    {
+        public ApplicationUser()
+        {
+            Diaryentries = new HashSet<Diaryentry>();
+        }
+
+        public string? City { get; set; }
+
+        public virtual ICollection<Diaryentry> Diaryentries { get; set; }
+    }   
+}
---
+++ MemoriKeeper.Model/Models/Diaryentry.cs
@@ -0,0 +1,14 @@
+﻿namespace MemoriKeeper.Model.Models
+{
+    public class Diaryentry
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public int AttachmentId { get; set; }
+        public string Description { get; set; }
+        public string ApplicationUserId { get; set; }
+
+        public virtual Attachment Attachment { get; set; }
+        public virtual ApplicationUser ApplicationUser { get; set; }
+    }
+}

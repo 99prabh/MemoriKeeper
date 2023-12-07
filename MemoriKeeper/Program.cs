2b2e240f2a45330f131736da38b3a4590f06029d
99prabh <133125732+99prabh@users.noreply.github.com>
2023-12-06 21:50:23 -0500

    Added some default images

--- MemoriKeeper/Program.cs
+++ MemoriKeeper/Program.cs
@@ -45,6 +45,6 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+    pattern: "{controller=FileType}/{action=Index}/{id?}");
 
 app.Run();

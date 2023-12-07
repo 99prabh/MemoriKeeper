324657934fdfae6f32335b54aa506432920c8850
99prabh <133125732+99prabh@users.noreply.github.com>
2023-12-06 21:49:25 -0500

    Created attachement modelview

---
+++ MemoriKeeper.Model/ViewModels/AttachmentCreateModel.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MemoriKeeper.Model.ViewModels
+{
+    public class AttachmentCreateModel
+    {
+        [Required(ErrorMessage = "Please, select type.")]
+        public int FileTypeId { get; set; }
+
+        [Required(ErrorMessage = "Please, provied file path.")]
+        [StringLength(500, MinimumLength = 2)]
+        public string FilePath { get; set; }
+
+        public string Description { get; set; }
+    }
+}

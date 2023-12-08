3438203467b10edf386dbd745c7397ed955cae02
99prabh <133125732+99prabh@users.noreply.github.com>
2023-12-07 21:04:00 -0500

    Added attachmentcontroller test class

---
+++ MemoriKeeper.UnitTest/AttachmentControllerUnitTest.cs
@@ -0,0 +1,75 @@
+﻿using MemoriKeeper.Controllers;
+using MemoriKeeper.Model.DatabaseContect;
+using MemoriKeeper.Model.Models;
+
+namespace MemoriKeeper.UnitTest
+{
+    [TestClass]
+    public class AttachmentControllerUnitTest
+    {
+        [TestMethod]
+        public void TestForIndex()
+        {
+            var attachments = new List<Attachment>();
+
+            // Arrange
+            var dbContext = new MemoriKeeperContext();
+            var attachmentController = new AttachmentController(dbContext, null);
+
+            // Act
+            var viewResult = attachmentController.Index();
+
+            // Assert
+            Assert.AreEqual<ICollection<Attachment>>(attachments, attachments);
+        }
+
+
+        [TestMethod]
+        public void TestForCreate()
+        {
+            var attachment = new Attachment();
+
+            // Arrange
+            var dbContext = new MemoriKeeperContext();
+            var attachmentController = new AttachmentController(dbContext, null);
+
+            // Act
+            var viewResult = attachmentController.Create();
+
+            // Assert
+            Assert.AreEqual<Attachment>(attachment, attachment);
+        }
+
+        [TestMethod]
+        public void TestForDetails()
+        {
+            var attachment = new Attachment();
+
+            // Arrange
+            var dbContext = new MemoriKeeperContext();
+            var attachmentController = new AttachmentController(dbContext, null);
+
+            // Act
+            var viewResult = attachmentController.Details(1);
+
+            // Assert
+            Assert.AreEqual<Attachment>(attachment, attachment);
+        }
+
+        [TestMethod]
+        public void TestForDelete()
+        {
+            var attachment = new Attachment();
+
+            // Arrange
+            var dbContext = new MemoriKeeperContext();
+            var attachmentController = new AttachmentController(dbContext, null);
+
+            // Act
+            var viewResult = attachmentController.Delete(1);
+
+            // Assert
+            Assert.AreEqual<Attachment>(attachment, attachment);
+        }
+    }
+}

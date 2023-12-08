e34b817091865d317f589d678172052734a1caef
99prabh <133125732+99prabh@users.noreply.github.com>
2023-12-07 20:59:43 -0500

    Created model class for attachments

--- MemoriKeeper.Model/Models/Attachment.cs
+++ MemoriKeeper.Model/Models/Attachment.cs
@@ -11,15 +11,18 @@ namespace MemoriKeeper.Model.Models
 
         public int Id { get; set; }
 
+        [Required(ErrorMessage = "Please, provied title.")]
+        [StringLength(300, MinimumLength = 2)]
+        public string Title { get; set; }
+
         [Required(ErrorMessage = "Please, select type.")]
-        public int TypeId { get; set; }
+        public int FileTypeId { get; set; }
 
         [Required(ErrorMessage = "Please, provied file path.")]
         [StringLength(500, MinimumLength = 2)]
         public string FilePath { get; set; }
-        public string Description { get; set; }
 
-        public virtual FileType FileType { get; set; }
-        public virtual ICollection<Diaryentry> Diaryentries { get; set; }
+        public FileType FileType { get; set; }
+        public ICollection<Diaryentry> Diaryentries { get; set; }
     }
 }

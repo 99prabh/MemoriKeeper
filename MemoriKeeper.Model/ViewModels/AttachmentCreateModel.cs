2a66faec2bc451ab1df1ee602cdb1d6eb3e7c5a2
99prabh <133125732+99prabh@users.noreply.github.com>
2023-12-07 21:00:26 -0500

    Updated viewmodel class with validation

--- MemoriKeeper.Model/ViewModels/AttachmentCreateModel.cs
+++ MemoriKeeper.Model/ViewModels/AttachmentCreateModel.cs
@@ -4,13 +4,15 @@ namespace MemoriKeeper.Model.ViewModels
 {
     public class AttachmentCreateModel
     {
+        [Required(ErrorMessage = "Please, provied title.")]
+        [StringLength(300, MinimumLength = 2)]
+        public string Title { get; set; }
+
         [Required(ErrorMessage = "Please, select type.")]
         public int FileTypeId { get; set; }
 
         [Required(ErrorMessage = "Please, provied file path.")]
         [StringLength(500, MinimumLength = 2)]
         public string FilePath { get; set; }
-
-        public string Description { get; set; }
     }
 }

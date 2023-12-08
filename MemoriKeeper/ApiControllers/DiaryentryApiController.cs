888592f44050509fe34fb4bf6284decf2c72784a
babitarawat <A00280963@mycambrian.ca>
2023-12-07 22:24:32 -0500

    create a dairy entry controller and add the service code to add dairy entry

---
+++ MemoriKeeper/ApiControllers/DiaryentryApiController.cs
@@ -0,0 +1,81 @@
+﻿using MemoriKeeper.Model.DatabaseContect;
+using MemoriKeeper.Model.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace MemoriKeeper.ApiControllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DiaryentryApiController : ControllerBase
+    {
+        private readonly MemoriKeeperContext _dbContext;
+        public DiaryentryApiController(MemoriKeeperContext dbContext) => _dbContext = dbContext;
+
+        [HttpGet]
+        public async Task<ActionResult<ICollection<Diaryentry>>> GetDiaryentries()
+        {
+            var diaryentries = await _dbContext.Diaryentries.Include(a => a.Attachment).ToListAsync();
+            return Ok(diaryentries);
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<Diaryentry>> GetDiaryentry(int id)
+        {
+            if (id == 0)
+                return BadRequest();
+
+            var existDiaryentry = await _dbContext.Diaryentries.Where(a => a.Id == id).FirstOrDefaultAsync();
+
+            if (existDiaryentry == null)
+                return BadRequest();
+
+            return Ok(existDiaryentry);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<Diaryentry>> Create(Diaryentry diaryentryModel)
+        {
+            if (ModelState.IsValid)
+            {
+                _dbContext.Diaryentries.Add(diaryentryModel);
+                await _dbContext.SaveChangesAsync();
+
+                return Ok(diaryentryModel);
+            }
+
+            return BadRequest();
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<Diaryentry>> Update(Diaryentry diaryentryModel)
+        {
+            if (ModelState.IsValid)
+            {
+                _dbContext.Diaryentries.Update(diaryentryModel);
+                await _dbContext.SaveChangesAsync();
+
+                return Ok(diaryentryModel);
+            }
+
+            return BadRequest();
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<int>> Delete(int id)
+        {
+            if (id == 0)
+                return BadRequest();
+
+            var existDiaryentry = await _dbContext.Diaryentries.Where(a => a.Id == id).FirstOrDefaultAsync();
+
+            if (existDiaryentry == null)
+                return BadRequest();
+
+            _dbContext.Diaryentries.Remove(existDiaryentry);
+            await _dbContext.SaveChangesAsync();
+
+            return Ok(id);
+        }
+    }
+}

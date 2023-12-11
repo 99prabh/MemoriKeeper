3b1c2aa67834aa85e651c26384dca08c74c288e2
babitarawat <A00280963@mycambrian.ca>
2023-12-11 14:10:30 -0500

    edit validations for dairy entry

--- MemoriKeeper.Model/Models/Diaryentry.cs
+++ MemoriKeeper.Model/Models/Diaryentry.cs
@@ -1,14 +1,25 @@
-﻿namespace MemoriKeeper.Model.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MemoriKeeper.Model.Models
 {
     public class Diaryentry
     {
         public int Id { get; set; }
-        public string Title { get; set; }
-        public int AttachmentId { get; set; }
-        public string Description { get; set; }
-        public string ApplicationUserId { get; set; }
 
-        public virtual Attachment Attachment { get; set; }
-        public virtual ApplicationUser ApplicationUser { get; set; }
+        [Required(ErrorMessage = "Please, provied title.")]
+        [StringLength(300, MinimumLength = 2)]
+        public string Title { get; set; }
+
+        [Required(ErrorMessage = "Please, select attachment file")]
+        public int AttachmentId { get; set; }
+
+        [Required(ErrorMessage = "Please, provied description of this attachment.")]
+        [StringLength(1500, MinimumLength = 2)]
+        public string Description { get; set; }
+
+        public string? ApplicationUserId { get; set; }
+
+        public Attachment Attachment { get; set; }
+        public ApplicationUser ApplicationUser { get; set; }
     }
 }

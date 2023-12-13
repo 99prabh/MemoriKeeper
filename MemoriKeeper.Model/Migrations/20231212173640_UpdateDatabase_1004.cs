50cd76ab67b645ccb17c9a6512993c5cc8a9e6d4
99prabh <133125732+99prabh@users.noreply.github.com>
2023-12-13 13:02:23 -0500

    Added more migrations

---
+++ MemoriKeeper.Model/Migrations/20231212173640_UpdateDatabase_1004.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MemoriKeeper.Model.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateDatabase_1004 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "CreatedDateTime",
+                table: "Diaryentries",
+                type: "datetime2",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "DeletedDateTime",
+                table: "Diaryentries",
+                type: "datetime2",
+                nullable: true);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsDeleted",
+                table: "Diaryentries",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CreatedDateTime",
+                table: "Diaryentries");
+
+            migrationBuilder.DropColumn(
+                name: "DeletedDateTime",
+                table: "Diaryentries");
+
+            migrationBuilder.DropColumn(
+                name: "IsDeleted",
+                table: "Diaryentries");
+        }
+    }
+}

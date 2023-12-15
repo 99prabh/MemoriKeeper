f919dee10fab8dde86b80b605584f4a61449b87a
babitarawat <A00280963@mycambrian.ca>
2023-12-14 23:46:16 -0500

    update dairy entry model and add isDeleted paramter

--- MemoriKeeper.Model/Models/Diaryentry.cs
+++ MemoriKeeper.Model/Models/Diaryentry.cs
@@ -19,6 +19,8 @@ namespace MemoriKeeper.Model.Models
 
         public string? ApplicationUserId { get; set; }
         public DateTime? CreatedDateTime { get; set; }
+        public bool IsDeleted { get; set; }
+        public DateTime? DeletedDateTime { get; set; }
 
         public Attachment Attachment { get; set; }
         public ApplicationUser ApplicationUser { get; set; }
